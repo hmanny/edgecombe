60dcb06e08de1fdda3560d0fadd56d7c53e2afc9
hmanny <harry@smarterwebcompany.co.uk>
2015-09-04 17:23:56 +0100

    end of day 04/09

--- edgecumbe-health-co-uk/_img/js/mobile-menu.js
+++ edgecumbe-health-co-uk/_img/js/mobile-menu.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
+    
+    $(".mobile-menu li").find('ul').wrap("<div class='innerMenu'></div>");
 
     var menu_up = 'notUp';
     $(".menu_button").on("click", function () {
         if (menu_up === 'notUp') {
-            $(".mobile-menu").fadeIn(200);
+            $(".mobile-menu").addClass("showMenu");
+            $(".site").css("overflow","hidden");
             $(this).addClass("is-open");
 
             menu_up = 'up';
         } else if (menu_up === 'up') {
-            $(".mobile-menu").fadeOut(200);
-            $(".mobile-menu li").find('ul').fadeOut(200);
+            $(".mobile-menu").removeClass("showMenu");
+            $(".mobile-menu li").find('.innerMenu').removeClass("showMenu");
             $(this).removeClass("is-open");
+            $(".site").removeAttr("style");
             menu_up = 'notUp';
         } else if (menu_up === 'lv2') {
-            $(".mobile-menu li").find('ul').fadeOut(200);
+            $(".mobile-menu li").find('.innerMenu').removeClass("showMenu");
             $(this).removeClass("level2")
             $(this).addClass("is-open");
 
@@ -21,21 +25,22 @@ $(document).ready(function () {
         }
     });
     $(window).on('resize', function () {
-        $(".mobile-menu").fadeOut();
-        $(".mobile-menu li").find('ul').fadeOut(200);
+        $(".mobile-menu").removeClass("showMenu");
+        $(".mobile-menu li").find('.innerMenu').removeClass("showMenu");
         $(".menu_button").removeClass("is-open");
         $(".menu_button").removeClass("level2");
-
+        $(".site").removeAttr("style");
         menu_up = 'notUp';
     });
 
 
-    $(".mobile-menu a").not(".nodiv").on('click', function () {
-        $(this).parent("li").find('ul').slideDown(200);
+    $(".mobile-menu li").not(".nodiv").find("a").on('click', function () {
+        $(this).parent("li").find('.innerMenu').addClass("showMenu");
         menu_up = "lv2";
         $(".menu_button").addClass("level2");
 
     });
+    $(".mobile-menu").children("ul").append($(".login"));
 
 
 
